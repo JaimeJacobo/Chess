2ff5a7dc2f2c1406bb88cf1e44a8a55ec4eff868
Jaime Jacobo <jaimejacobordv@gmail.com>
2019-01-26 21:21:20 -0500

    making changes

--- script.js
+++ script.js
@@ -1,2 +1,53 @@
 
 
+//Clicking on the black pawns
+
+// $('#firstBlackPawn').click(function(){
+//   $(this).addClass('pulse');
+//   console.log('bubucela');
+//   if($(this).parent().attr('id') === '01'){
+//     console.log('satania');
+//     $('#02').click(function(){
+//       $('#01').html('');
+//       $('#02').append('<img src="Chess Images/blackPeon.png" alt="" class="figureSize" id="firstBlackPawn">');
+//     });
+//   };
+// });
+
+// $('#secondBlackPawn').click(function(){
+//   $(this).addClass('pulse');
+// });
+
+// $('#thirdBlackPawn').click(function(){
+//   $(this).addClass('pulse');
+// });
+
+// $('#fourthBlackPawn').click(function(){
+//   $(this).addClass('pulse');
+// });
+
+// $('#fifthBlackPawn').click(function(){
+//   $(this).addClass('pulse');
+// });
+
+// $('#sixthBlackPawn').click(function(){
+//   $(this).addClass('pulse');
+// });
+
+// $('#seventhBlackPawn').click(function(){
+//   $(this).addClass('pulse');
+// });
+
+// $('#eighthBlackPawn').click(function(){
+//   $(this).addClass('pulse');
+// });
+
+
+
+let firstPawn = {
+  alive: true,
+  color: 'black',
+  type: 'pawn',
+  position: {x:0, y:1},
+  image: 'Chess Images/blackPeon.png'
+};
